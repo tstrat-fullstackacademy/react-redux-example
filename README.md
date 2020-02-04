9d92fc2002fb1162671d7e89a943f12a2d461788
Travis Stratton <t.strat7@gmail.com>
2020-02-03 23:10:17 -0600

    Inertial kermit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Steps to instal
+
+1. npm init -y
+2. npm install react react-dom
+3. npm install --save-dev webpack webpack-cli webpack-dev-server
+4. Add start script to package.json
+
+```
+"scripts": { "start": "webpack-dev-server --mode development"}
+```
+
+5.
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: "./client/index.js",
+  output: {
+    path: __dirname,
+    filename: "./public/bundle.js"
+  },
+  mode: "development",
+  context: __dirname,
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.(s*)css$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
+};
